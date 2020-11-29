25b7df9c13e51bbd17618a49c1d6d80687d4a05c
Rohini Senthil <rohinivsenthil@gmail.com>
2020-11-29 20:37:09 +0530

    (feat) add icons

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  library.add(faGithub, faTwitter, faInstagram, faEnvelope)
+  Vue.component('font-awesome', FontAwesomeIcon)
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;500;600;700&family=Roboto&display=swap'
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,13 +4,24 @@
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
-
-    <div class="message">
+    <div class="wrap">
+      <div class="message">
     <div class="subtext">This is me</div>
     </div>
-    <div class="end"/>
     <div class="name">Rohini Senthil</div>
-    <div class="tagline">Software <div style="color: black">Developer</div> and Self-taught <div style="color: black">Artist</div></div>
+    <div class="tagline">Software <div style="color: #03a9f4">Developer</div> and Self-taught <div style="color: #03a9f4">Artist</div></div>
+   <div class="align">
+    <div class="message2">
+    <div class="subtext2">Let's get in touch</div>
+    </div>
+    <div class="icons">
+      <font-awesome :icon="['fab', 'github']" style="margin: 6px; font-size: 22px" title="Github"/>
+      <font-awesome :icon="['fab', 'twitter']" style="margin: 6px; font-size: 22px" title="Twitter"/>
+      <font-awesome :icon="['fab', 'instagram']" style="margin: 6px; font-size: 22px" title="Instagram"/>
+      <font-awesome :icon="['fas', 'envelope']" style="margin: 6px 0px 6px 6px; font-size: 22px" title="Mail"/>
+    </div>
+    </div>
+    </div>
     </div>
 
   </Layout>
@@ -30,7 +41,15 @@ export default {
 }
 
 .common{
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: 'Poppins', sans-serif;
+  border-width: 0px 0px 2px 0px;
+  border-color: #e0e0e0;
+  border-style: solid;
+  padding-top: 70px;
+  padding-bottom: 70px;
 }
 
 .name{
@@ -38,10 +57,19 @@ export default {
   font-weight: 700;
 }
 
+.icons {
+  margin-top: 6px;
+}
+
 .tagline {
   display: flex;
   white-space: pre;
   color: #757575;
+}
+.align {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 }
 
 .subtext{
@@ -50,12 +78,32 @@ export default {
   width:fit-content;
 }
 
+.wrap {
+  width: 100%;
+}
+
 .message {
   background-color: #e0e0e0;
   padding: 8px;
   border-radius: 10px 15px 15px 0px;
   color: black;
   width: fit-content;
+}
+
+.subtext2{
+  font-size: 13px;
+  font-weight: 500;
+  width:fit-content;
+}
+
+.message2 {
+  margin-top: 16px;
+  background-color: #03a9f4;
+  padding: 10px;
+  border-radius: 15px 10px 0px 15px;
+  color: white;
+  width: fit-content;
+  /* text-decoration: underline; */
 }
 
 </style>
