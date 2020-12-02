ecf06de7dc43a102f0c74d600f02e5f861bbd7f6
Rohini Senthil <rohinivsenthil@gmail.com>
2020-12-02 15:12:22 +0530

    (improv) style blog page

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,11 +1,15 @@
 <template>
   <Layout>
     <div class="blog-container">
-      <div class="section-wrapper">
-        <TextBubble message="Check out my blogs" :sender="false"/>
+      <div class="header-wrapper">
+        <TextBubble message="Read my thoughts and learnings" :sender="false"/>
+        <div class="header-title">Blogs</div>
+        <div class="header-subtitle">Alternatively, you can also find my writings on 
+          <a href="https://dev.to/rohinivsenthil" target="__blank" class="external-link text-highlight">dev.to</a>
+        </div>
       </div>
+      <Separator/>
     </div>
-    <Separator/>
   </Layout>
 </template>
 
@@ -30,5 +34,21 @@ export default {
   justify-content: center;
   align-items: center;
   font-family: 'Poppins', sans-serif;
+}
+
+.header-wrapper {
+  width: 100%;
+  padding: 20px 0px 40px 0px;
+}
+
+.header-title {
+  font-size: 42px;
+  font-weight: 700;
+  margin: 12px 0px 10px 0px;
+}
+
+.header-subtitle {
+  color: #757575;
+  margin-bottom: 20px;
 }
 </style>
